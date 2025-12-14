582a3e3a7263705a3995876a5f7f6a5bd81ac2ee
rasult22 <warrior.006@inbox.ru>
2025-12-14 18:27:58 +0500

    feat: Add Editor.js module type declarations and a backup JSON file.

---
+++ src/types/editorjs-modules.d.ts
@@ -0,0 +1,10 @@
+declare module '@editorjs/checklist';
+declare module '@editorjs/header';
+declare module '@editorjs/list';
+declare module '@editorjs/paragraph';
+declare module '@editorjs/quote';
+declare module '@editorjs/code';
+declare module '@editorjs/inline-code';
+declare module '@editorjs/marker';
+declare module '@editorjs/delimiter';
+
